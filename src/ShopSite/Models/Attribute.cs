191c6d052f8aa451c6e5901d39047205f40c97db
Romanas <gensatus@gmail.com>
2017-04-03 07:33:55 +0300

    Added attribute model

---
+++ src/ShopSite/Models/Attribute.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopSite.Models
+{
+    public class Attribute
+    {
+        public int Id { get; set; }
+
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+
+        public virtual AttributeGroup Group { get; set; }
+    }
+}
