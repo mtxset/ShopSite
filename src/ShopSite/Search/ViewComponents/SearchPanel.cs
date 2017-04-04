c224158a11de7e9a21d49f76b38c422e668ee064
Leonas <master@puppets.com>
2017-04-04 19:58:52 +0300

    Starting Search Panel

---
+++ src/ShopSite/Search/ViewComponents/SearchPanel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopSite.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopSite.ViewComponents
+{
+    public class SearchPanel : ViewComponent
+    {
+        private ICategoryService _categoryService;
+        private IProductService _productService;
+
+        public SearchPanel(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            // TODO: Unique Search for product and category
+            return View();
+        }
+    }
+}
+
