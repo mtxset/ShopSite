39cacad45606392b87485a827188385bfd4f6b32
yuk <master@puppets.com>
2017-04-05 15:07:52 +0300

    Migration

---
+++ src/ShopSite/Migrations/20170405120306_primary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopSite.Migrations
+{
+    public partial class primary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
